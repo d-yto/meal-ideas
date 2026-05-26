17998cb438660573d52838f88b70577b7f901396
Aidan Wallace <49172003@edu.cbe.ab.ca>
2026-05-26 10:38:18 -0600

    github is frustrating somtimes

--- js/weather.js
+++ js/weather.js
@@ -3,15 +3,25 @@ const WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
 
 
 async function getLocation(){
-    try {
-        let response = await fetch(
-            `${GEO_URL}?name=${encodeURIComponent(city)}&count=1`)
-        if (!response.ok) throw new Error(
-            `Geocoding failed: ${response.status} `)
+    let response = await fetch(
+        `${GEO_URL}?name=${encodeURIComponent(city)}&count=1`)
+    if (!response.ok) throw new Error(
+        `Geocoding failed: ${response.status} `)
 
-        let data = await response.json()
-        if(!data.results || data.response === 0){
-            throw new Error(`city: ${city} not found`)
-        }
+    let data = await response.json()
+    let results = data.results
+    if(!results || data.response === 0){
+        throw new Error(`city: ${city} not found`)
     }
+    return {
+        name:results[0].name,
+        country:results[0].country,
+        latitude:results[0].latitude,
+        longitude:results[0].longitude,
+    }
+
+}
+
+async function getWeather() {
+    
 }
